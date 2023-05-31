ad2d691231500043fef634ed4ec576f9b79b9e09
Sojeong Park <creerari01@gmail.com>
2023-05-31 18:01:14 +0900

    .

--- src/main/java/com/flab/marketflea/MarketfleaApplication.java
+++ src/main/java/com/flab/marketflea/MarketfleaApplication.java
@@ -2,12 +2,10 @@ package com.flab.marketflea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableAsync
+@EnableCaching
 public class MarketfleaApplication {
 
     public static void main(String[] args) {
